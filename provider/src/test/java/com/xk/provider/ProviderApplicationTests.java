1c397f5bd0343fe71d63d4ff361094f752576b0d
xk-dragonfly <xk0708666@gmail.com>
2024-08-17 14:00:26 +0800

    创建消费者与服务提供者模块

---
+++ provider/src/test/java/com/xk/provider/ProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xk.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
